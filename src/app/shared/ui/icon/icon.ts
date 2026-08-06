56dd3d0a9ccb42358b9acd1721542dda7a0d493b
hachavez95 <hachavez@protonmail.com>
2026-08-06 18:25:48 -0500

    refactor: Mejorar estilizacion de íconos

--- src/app/shared/ui/icon/icon.ts
+++ src/app/shared/ui/icon/icon.ts
@@ -1,13 +1,15 @@
 import { Component, input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
     selector: 'app-icon',
-    imports: [],
+    imports: [NgClass],
     standalone: true,
     templateUrl: './icon.html',
     styleUrl: './icon.css',
 })
 export class Icon {
     name = input.required<string>();
-    size = input<number>(24);
+    size = input<string>('size-6');
+    color = input<string>('text-white');
 }
